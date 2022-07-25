ee6a96eb4ac5118002187ec3dcc1f0259cadba8c
Syd Amir <hemedani@gmail.com>
2022-07-25 19:12:13 +0430

    todos url implement

--- index.ts
+++ index.ts
@@ -3,7 +3,20 @@ import http, { ServerResponse, ClientRequest } from "http";
 const host = "localhost";
 const port = 8000;
 
-const requestListener = function (req: ClientRequest, res: ServerResponse) {
+let todos = [
+  { id: 1, title: "Learn TypeScript", completed: false },
+  { id: 2, title: "Learn React", completed: false },
+  { id: 3, title: "Learn Angular", completed: false },
+  { id: 4, title: "Learn Node", completed: false },
+  { id: 5, title: "Learn MongoDB", completed: false },
+  { id: 6, title: "Learn GraphQL", completed: false },
+];
+const requestListener = function (req: Request, res: ServerResponse) {
+  if (req.url === "/todos") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(todos));
+  }
+
   res.writeHead(200);
   res.end("My first server!");
 };
